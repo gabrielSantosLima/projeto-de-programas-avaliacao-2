3b51ca017712e4d5a65b026dbee671c2c2f4e80c
Melinne Diniz Oliveira <61233511+melinnediniz@users.noreply.github.com>
2022-09-03 23:34:31 -0300

    Questoes pares

--- src/br/edu/uea/pp/iu/CaixaEletronico.java
+++ src/br/edu/uea/pp/iu/CaixaEletronico.java
@@ -13,10 +13,15 @@ public class CaixaEletronico { // Questão 2 - a)
 	
 	public static void main(String[] args) { 
 		ContaCorrente contaCorrente = new ContaCorrente(123456789, 001); // Questão 2 - b) TODO Instanciar também a classe Cliente
+		contaCorrente.depositar(1200); // Questão  2 - c
+		contaCorrente.sacar(200); // Questão 2 - d
 		
 		
 		ArrayList<Transacao> extrato = contaCorrente.getExtrato(); // Questão 2 - f)
 		System.out.println("======= CONTA CORRENTE =======");
+		System.out.println("| AGÊNCIA: " + contaCorrente.getNumeroAgencia() 
+		+ "\n| NÚMERO DA CONTA: " + contaCorrente.getNumeroConta() 
+		+ "\n| SALDO ATUAL: R$" + contaCorrente.getSaldo()+ "\n");
 		if(extrato.isEmpty()) {
 			System.out.println("Nenhuma transação realizada no momento.");
 		}else {
